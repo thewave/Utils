8d916d9fedaabfbecd95d3a49d8776454e8cf7fc
christianpeixoto <christianpeixoto@gmail.com>
2011-07-25 23:38:23 -0300

    documentacao

--- src/br/com/brasilti/utils/collection/CollectionUtil.java
+++ src/br/com/brasilti/utils/collection/CollectionUtil.java
@@ -3,8 +3,23 @@ package br.com.brasilti.utils.collection;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Agrupa metodos estaticos com o objetivo de facilitar a manipulacao de colecoes.
+ * 
+ * @author Benedito Barbosa
+ * @author Christian Peixoto
+ * 
+ */
 public class CollectionUtil {
 
+	/**
+	 * Converte um determinado varargs em uma lista. Cada elemento do varargs ocupara a mesma posicao na lista retornada.
+	 * 
+	 * @param varargs
+	 * @return Lista de elementos.
+	 * @exception IllegalArgumentException
+	 *                se o varargs for nulo.
+	 */
 	public static <T> List<T> convert(T... varargs) {
 		if (varargs == null) {
 			throw new IllegalArgumentException();
--- src/br/com/brasilti/utils/reflection/ReflectionUtil.java
+++ src/br/com/brasilti/utils/reflection/ReflectionUtil.java
@@ -9,24 +9,56 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+/**
+ * Agrupa metodos estaticos com o objetivo de facilitar o uso de reflexao.
+ * 
+ * @author Benedito Barbosa
+ * @author Christian Peixoto
+ * 
+ */
 public class ReflectionUtil {
 
 	private ReflectionUtil() {
 
 	}
 
+	/**
+	 * Indica que um determinado atributo e estatico.
+	 * 
+	 * @param field
+	 * @return true se o atributo e estatico.
+	 */
 	public static boolean isStatic(Field field) {
 		return Modifier.isStatic(field.getModifiers());
 	}
 
+	/**
+	 * Indica que um determinado atributo e transiente.
+	 * 
+	 * @param field
+	 * @return true se o atributo e transiente.
+	 */
 	public static boolean isTransient(Field field) {
 		return Modifier.isTransient(field.getModifiers());
 	}
 
+	/**
+	 * Indica que um determinado atributo e persistente. Um atributo e persistente quando nao e estatico nem transiente.
+	 * 
+	 * @param field
+	 * @return true se o atributo e persistente.
+	 */
 	public static boolean isPersistent(Field field) {
 		return !isStatic(field) && !isTransient(field);
 	}
 
+	/**
+	 * Retorna os atributos persistentes de uma determinada classe.
+	 * 
+	 * @param klass
+	 * @return Lista de atributos.
+	 * @see #isPersistent(Field)
+	 */
 	public static List<Field> getPersistentFields(Class<?> klass) {
 		List<Field> fields = new ArrayList<Field>();
 
@@ -39,6 +71,13 @@ public class ReflectionUtil {
 		return fields;
 	}
 
+	/**
+	 * Indica que a classe implementa uma determinada interface.
+	 * 
+	 * @param klass
+	 * @param interfaceClass
+	 * @return true se a classe implementa a interface.
+	 */
 	public static boolean implementz(Class<?> klass, Class<?> interfaceClass) {
 		Class<?>[] interfaces = klass.getInterfaces();
 		for (Class<?> i : interfaces) {
@@ -50,6 +89,13 @@ public class ReflectionUtil {
 		return false;
 	}
 
+	/**
+	 * Retorna o valor de um determinado atributo da instancia.
+	 * 
+	 * @param field
+	 * @param instance
+	 * @return null se {@link IllegalArgumentException} ou {@link IllegalAccessException} for capturada.
+	 */
 	public static <T> Object get(Field field, T instance) {
 		try {
 			boolean accessible = field.isAccessible();
@@ -67,6 +113,13 @@ public class ReflectionUtil {
 		return null;
 	}
 
+	/**
+	 * Atribui valor a um determinado atributo da instancia.
+	 * 
+	 * @param value
+	 * @param field
+	 * @param instance
+	 */
 	public static <T> void set(Object value, Field field, T instance) {
 		try {
 			boolean accessible = field.isAccessible();
@@ -82,6 +135,14 @@ public class ReflectionUtil {
 		}
 	}
 
+	/**
+	 * Copia o valor de cada atributo do objeto de origem para o objeto de destino.
+	 * 
+	 * @param origin
+	 * @param target
+	 * @exception IllegalArgumentException
+	 *                se a classe do objeto de origem for diferente da classe do objeto de destino.
+	 */
 	public static <T> void copy(T origin, T target) {
 		Class<?> originClass = origin.getClass();
 		Class<?> targetClass = target.getClass();
@@ -97,9 +158,17 @@ public class ReflectionUtil {
 		}
 	}
 
+	/**
+	 * Retorna o tipo dos elementos de um determinado atributo.
+	 * 
+	 * @param field
+	 * @return Tipo dos elementos de uma colecao.
+	 * @exception IllegalArgumentException
+	 *                se o atributo nao for uma colecao.
+	 * @see #isCollection(Class)
+	 */
 	public static Class<?> getTypeOfElements(Field field) {
-		boolean isNotCollection = !field.getType().equals(Collection.class);
-		if (isNotCollection) {
+		if (!isCollection(field.getType())) {
 			throw new IllegalArgumentException();
 		}
 
@@ -108,6 +177,12 @@ public class ReflectionUtil {
 		return (Class<?>) type.getActualTypeArguments()[0];
 	}
 
+	/**
+	 * Indica que uma determinada classe e colecao
+	 * 
+	 * @param klass
+	 * @return true se a classe e colecao.
+	 */
 	public static boolean isCollection(Class<?> klass) {
 		boolean isArrayList = klass.equals(ArrayList.class);
 		boolean isList = klass.equals(List.class);
@@ -116,25 +191,35 @@ public class ReflectionUtil {
 		return isArrayList || isList || isCollection;
 	}
 
+	/**
+	 * Indica que a classe esta anotada por uma determinada anotacao.
+	 * 
+	 * @param klass
+	 * @param annotationClass
+	 * @return true se a classe esta anotada pela anotacao.
+	 */
 	public static boolean isAnnotated(Class<?> klass, Class<? extends Annotation> annotationClass) {
 		return klass.getAnnotation(annotationClass) != null;
 	}
 
+	/**
+	 * Indica que o atributo esta anotado por uma determinada anotacao.
+	 * 
+	 * @param field
+	 * @param annotationClass
+	 * @return true se o atributo esta anotado pela anotacao.
+	 */
 	public static boolean isAnnotated(Field field, Class<? extends Annotation> annotationClass) {
 		return field.getAnnotation(annotationClass) != null;
 	}
 
-	public static boolean hasField(Class<?> klass, String fieldName) {
-		Field[] fields = klass.getDeclaredFields();
-		for (Field field : fields) {
-			if (field.getName().equals(fieldName)) {
-				return true;
-			}
-		}
-
-		return false;
-	}
-
+	/**
+	 * Retorna o atributo de uma determinada classe.
+	 * 
+	 * @param fieldName
+	 * @param klass
+	 * @return null se {@link SecurityException} ou {@link NoSuchFieldException} for capturada.
+	 */
 	public static Field getField(String fieldName, Class<?> klass) {
 		try {
 			return klass.getDeclaredField(fieldName);
@@ -147,6 +232,12 @@ public class ReflectionUtil {
 		return null;
 	}
 
+	/**
+	 * Indica que a classe possui um construtor padrao sem argumentos.
+	 * 
+	 * @param klass
+	 * @return true se a classe possui um construtor.
+	 */
 	public static boolean hasConstructor(Class<?> klass) {
 		try {
 			return klass.getConstructor() != null;
@@ -159,6 +250,31 @@ public class ReflectionUtil {
 		return false;
 	}
 
+	/**
+	 * Indica que a classe possui um determinado atributo.
+	 * 
+	 * @param klass
+	 * @param fieldName
+	 * @return true se a classe possui o atributo.
+	 */
+	public static boolean hasField(Class<?> klass, String fieldName) {
+		Field[] fields = klass.getDeclaredFields();
+		for (Field field : fields) {
+			if (field.getName().equals(fieldName)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * Indica que a classe possui um determinado metodo.
+	 * 
+	 * @param klass
+	 * @param methodName
+	 * @return true se a classe possui o metodo.
+	 */
 	public static boolean hasMethod(Class<?> klass, String methodName) {
 		Method[] methods = klass.getDeclaredMethods();
 		for (Method method : methods) {
